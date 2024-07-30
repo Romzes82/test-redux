f15829e6348e027defa0d6263cc250c94ae1c576
Roman Maslennikov <romzes120282@gmail.com>
2024-07-30 18:39:23 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import UserPage from './features/users/user'
+
+function App() {
+  return (
+    <>
+      <UserPage/>
+    </>
+  )
+}
+
+export default App
